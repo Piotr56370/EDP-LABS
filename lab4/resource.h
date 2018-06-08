a039e191296cb0072ef3a1ad0536db0359abf9cc
Piotr56370 <39531255+Piotr56370@users.noreply.github.com>
2018-06-08 14:34:37 +0300

    Added labs

---
+++ lab4/resource.h
@@ -0,0 +1,23 @@
+#include <time.h>
+#define IDT_TIMER 100
+#include <cmath>
+#include <windows.h>
+
+class Ball
+{
+public:
+	POINT center;
+	COLORREF color;
+	int xVel, yVel;
+    int randomColor[3];
+    int shape; // 0 - circle, 1- rectangle
+
+	Ball(POINT, const int);
+	bool accelerateBall(const int, const int);
+	bool setColor(const COLORREF);
+	bool onBorderImpact(const RECT);
+	bool moveBall(const HDC, const RECT, HBRUSH);
+	COLORREF randColor();
+};
+
+bool checkBallCollision(Ball *b1, Ball *b2);
